93625c99f7583f98821b883e492ce10cdc498296
Andrius Mudinas <keistokas@gmail.com>
2019-01-10 23:36:33 +0000

    Changing deep learning

--- src/wikilednlp/learning/DeepLearning.py
+++ src/wikilednlp/learning/DeepLearning.py
@@ -1,17 +1,12 @@
 import abc
 import os
 from os import path, makedirs
-
-import gc
-from keras import backend as k
+from keras.initializers import Constant
+from keras import backend as k, Input, Model
 from keras import callbacks
-from keras.layers import Embedding, LSTM, Dropout, Dense, np, Flatten, Activation, \
-    Reshape, Convolution2D, MaxPooling2D
+from keras.layers import Embedding, Dropout, Dense, np, Activation, Conv1D, MaxPooling1D, GlobalMaxPooling1D
 from keras.models import Sequential
-from keras.optimizers import RMSprop
-from keras.regularizers import l2
 from pathlib2 import Path
-from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils import check_array
 
 import wikilednlp.utilities.Constants as Constants
@@ -27,45 +22,40 @@ class BaseDeepStrategy(object):
 
     counter = 0
 
-    def __init__(self, project_name, sub_project, doc_size):
+    def __init__(self, project_name, sub_project, max_length):
         self.counter = BaseDeepStrategy.counter
         BaseDeepStrategy.counter += 1
-        self.doc_size = doc_size
+        self.max_length = max_length
         self.project_name = project_name + "_" + sub_project
-        logger.info('%s with doc_size %i', project_name, doc_size)
+        logger.info('%s with doc_size %i', project_name, max_length)
         self.project_path = path.join(Constants.TEMP, 'Deep', project_name)
         self.epochs_number = 20
         self.model = None
+        self.early_stop = None
 
-    def add_embeddings(self, model):
+    def get_embeddings(self):
         vectors = self.loader.parser.word2vec.embedding_matrix
-        model.add(Embedding(input_dim=vectors.shape[0],
-                          output_dim=vectors.shape[1],
-                          input_length=self.doc_size,
-                          weights=[vectors],
-                          trainable=False))
+        embedding_layer = Embedding(vectors.shape[0],
+                                    vectors.shape[1],
+                                    embeddings_initializer=Constant(vectors),
+                                    input_length=self.max_length,
+                                    trainable=False)
+        return embedding_layer
 
     def add_output(self, model):
         model.add(Dropout(0.5))
         total_classes = self.loader.convertor.total_classes()
-        if self.loader.convertor.is_binary():
-            model.add(Dense(1))
-            model.add(Activation('sigmoid'))
-        else:
-            model.add(Dense(total_classes))
-            model.add(Activation('softmax'))
+        model.add(Dense(total_classes))
+        model.add(Activation('softmax'))
 
     def init_mode(self):
         if not hasattr(self, 'model') or self.model is None:
             self.model = self.construct_model()
             self.model.summary()
-            if self.loader.convertor.is_binary():
-                self.model.compile(loss="binary_crossentropy", optimizer=RMSprop(), metrics=["accuracy"])
-            else:
-                self.model.compile(loss="categorical_crossentropy", optimizer=RMSprop(), metrics=["accuracy"])
+            self.compile()
 
     def get_file_name(self, name):
-        file_path = path.join(self.project_path, 'weights', name, str(self.doc_size) + '_keras-lstm.h5')
+        file_path = path.join(self.project_path, 'weights', name, str(self.max_length) + '_keras-lstm.h5')
         weights = path.dirname(file_path)
         if not Path(weights).exists():
             makedirs(weights)
@@ -100,19 +90,18 @@ class BaseDeepStrategy(object):
     def predict_proba(self, test_x):
         logger.info('Predict with %i records', len(test_x))
         self.init_mode()
-        y = self.model.predict_proba(test_x, Constants.TESTING_BATCH)
+        y = self.model.predict(test_x, Constants.TESTING_BATCH)
         return y
 
     def predict(self, test_x):
         logger.info('Predict with %i records', len(test_x))
-        self.init_mode()
-        y = self.model.predict(test_x, Constants.TESTING_BATCH)
-        return y
+        y_prob = self.predict_proba()
+        return y_prob.argmax(axis=-1)
 
     def test_predict(self, test_x, test_y):
         logger.info('Test predict_proba with %i records', len(test_x))
         self.init_mode()
-        result_y_prob = self.model.predict_proba(test_x, batch_size=Constants.TESTING_BATCH, verbose=1)
+        result_y_prob = self.model.predict(test_x, batch_size=Constants.TESTING_BATCH, verbose=1)
         result_y = Utilities.make_single_dimension(result_y_prob)
         result_y_prob_single = self.loader.convertor.make_single(result_y_prob)
 
@@ -122,37 +111,33 @@ class BaseDeepStrategy(object):
         return result_y, result_y_prob_single
 
     def get_classes(self, y):
-        if self.loader.convertor.is_binary():
-            return np.unique(check_array(y, ensure_2d=False, allow_nd=True))
-        else:
-            single = Utilities.make_single_dimension(y)
-            return np.unique(check_array(single, ensure_2d=False, allow_nd=True))
+        single = Utilities.make_single_dimension(y)
+        return np.unique(check_array(single, ensure_2d=False, allow_nd=True))
+
+    def compile(self):
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+        self.model.summary()
 
     def fit(self, train_x, train_y):
         logger.info('Training with %i records', len(train_x))
         self.init_mode()
         Utilities.count_occurences(train_y)
-        cbks = [callbacks.EarlyStopping(monitor='val_loss', patience=2)]
+        train_x, train_y = Utilities.unison_shuffled_copies(train_x, train_y)
+        cbks = None
+        if self.early_stop is not None:
+            cbks = [callbacks.EarlyStopping(monitor='val_loss', patience=self.early_stop)]
         self.model.fit(train_x, train_y, batch_size=Constants.TRAINING_BATCH, callbacks=cbks, epochs=self.epochs_number,
                    validation_split=0.25, shuffle=True)
 
-    def construct_model(self):
-        model = Sequential()
-        return model
 
-    @abc.abstractmethod
-    def copy(self):
-        return None
+class CnnSentiment(BaseDeepStrategy):
 
-
-class ConvNetAlgorithm(BaseDeepStrategy):
-
-    def __init__(self, loader, project_name, doc_size, vocab_size=10000):
-        self.doc_size = doc_size
+    def __init__(self, loader, project_name, max_length, vocab_size=10000):
+        self.max_length = max_length
         self.vocab_size = vocab_size
         self.loader = loader
         self.word_vector_size = self.loader.parser.word2vec.vector_size
-        super(ConvNetAlgorithm, self).__init__(project_name, self.get_name(), doc_size)
+        super(CnnSentiment, self).__init__(project_name, self.get_name(), max_length)
 
     def get_name(self):
         return self.loader.parser.word2vec.name + '_OneDimensional'
@@ -163,61 +148,29 @@ class ConvNetAlgorithm(BaseDeepStrategy):
             raise NotImplemented("This model support dual data only")
 
         k.set_image_data_format('channels_first')
-        # Number of feature maps (outputs of convolutional layer)
-        n_fm = 500
-        # kernel size of convolutional layer
-        kernel_size = 8
-        conv_input_height = self.doc_size
-        conv_input_width = self.word_vector_size
 
-        logger.info('conv_input_height: %s conv_input_width: %s', conv_input_height, conv_input_width)
+        sequence_input = Input(shape=(self.max_length,), dtype='int32')
+        embedding_layer = self.get_embeddings()
+        embedded_sequences = embedding_layer(sequence_input)
+        x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+        x = MaxPooling1D(5)(x)
+        x = Conv1D(128, 5, activation='relu')(x)
+        x = MaxPooling1D(5)(x)
+        x = Conv1D(128, 5, activation='relu')(x)
+        x = GlobalMaxPooling1D()(x)
+        x = Dense(128, activation='relu')(x)
 
-        model = Sequential()
-        # Embedding layer (lookup table of trainable word vectors)
-        self.add_embeddings(model)
-        # Reshape word vectors from Embedding to tensor format suitable for Convolutional layer
-        model.add(Reshape((1, conv_input_height, conv_input_width)))
+        preds = Dense(2, activation='softmax')(x)
+        model = Model(sequence_input, preds)
 
-        # first convolutional layer
-        model.add(Convolution2D(n_fm,
-                                kernel_size,
-                                conv_input_width,
-                                border_mode='valid',
-                                W_regularizer=l2(0.0001)))
-        # ReLU activation
-        model.add(Activation('relu'))
-
-        # aggregate data in every feature map to scalar using MAX operation
-        model.add(MaxPooling2D(pool_size=(conv_input_height - kernel_size + 1, 1)))
-
-        model.add(Flatten())
         # Inner Product layer (as in regular neural network, but without non-linear activation function)
-        self.add_output(model)
-
         return model
 
-    def compile(self):
-        if self.loader.convertor.is_binary():
-            self.model.compile(loss="binary_crossentropy", optimizer='rmsprop', metrics=["accuracy"])
-        else:
-            self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.model.summary()
 
-    def copy(self):
-        copy_instance = ConvNetAlgorithm(self.loader, self.project_name, self.doc_size, self.vocab_size)
-        return copy_instance
-
-    def __copy__(self):
-        return self.copy()
-
-    def __deepcopy__(self, memo):
-        return self.copy()
-
-
-class BaselineLSTM(ConvNetAlgorithm):
+class BaselineLSTM(CnnSentiment):
 
     def get_name(self):
-        return self.loader.parser.word2vec.name + '_LTSM_' + str(self.doc_size)
+        return self.loader.parser.word2vec.name + '_LTSM_' + str(self.max_length)
 
     @abc.abstractmethod
     def construct_ltsm_model(self, model):
@@ -229,51 +182,3 @@ class BaselineLSTM(ConvNetAlgorithm):
         self.add_output(model)
         return model
 
-
-class WeightsLSTM(BaselineLSTM):
-
-    def __init__(self, loader, project_name, doc_size, vocab_size=10000, lstm=150):
-        self.lstm = lstm
-        super(WeightsLSTM, self).__init__(loader, project_name, doc_size, vocab_size)
-
-    def get_name(self):
-        vector_size = self.loader.parser.word2vec.embedding_matrix.shape[1]
-        return self.loader.parser.word2vec.name + '_LTSM_Weights_' + str(self.doc_size) + "_" + str(vector_size)
-
-    def construct_ltsm_model(self, model):
-        self.add_embeddings(model)
-        model.add(LSTM(self.lstm, return_sequences=False))
-
-    def copy(self):
-        copy_instance = WeightsLSTM(self.loader, self.project_name, self.doc_size, self.vocab_size, self.lstm)
-        return copy_instance
-
-    def __copy__(self):
-        return self.copy()
-
-    def __deepcopy__(self, memo):
-        return self.copy()
-
-
-class DeepSklearnWrapper(BaseEstimator, ClassifierMixin):
-
-    def __init__(self, classifier):
-        self.classifier = classifier
-
-    def fit(self, X, y):
-        y = self.classifier.loader.convertor.create_vector(y)
-        result = self.classifier.fit(X, y)
-        self.classifier.save('Multi_' + str(self.classifier.counter))
-        self.classes_ = self.classifier.get_classes(y)
-        del self.classifier.model
-        gc.collect()
-        return result
-
-    def predict_proba(self, X):
-        self.classifier.load("Multi_" + str(self.classifier.counter))
-        y = self.classifier.predict_proba(X)
-        if len(self.classes_) == 2:
-            y = Utilities.make_binary_prob(y)
-        del self.classifier.model
-        gc.collect()
-        return y
